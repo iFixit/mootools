e4fb59237feef66c4c6f2e739fe5c67f5179a7f4
David Rans <david@ifixit.com>
2023-03-23 09:12:04 -0700

    Drop Fx.SmoothScroll
    No longer needed as of https://github.com/iFixit/ifixit/pull/47048.

--- MooTools-More-1.6.0.js
+++ MooTools-More-1.6.0.js
@@ -1,6 +1,6 @@
 /* MooTools: the javascript framework. license: MIT-style license. copyright: Copyright (c) 2006-2022 [Valerio Proietti](https://mootools.net/).*/ 
 /*!
-Web Build: https://mootools.net/more/builder/4d05f603fa21b955faa7b3b562277fa2
+Web Build: https://mootools.net/more/builder/ce5983b5e1b9f86b88a68f873f781707
 */
 /*
 ---
@@ -3878,77 +3878,6 @@ Element.implement({
 		return this;
 	}
 
-});
-
-/*
----
-
-script: Fx.SmoothScroll.js
-
-name: Fx.SmoothScroll
-
-description: Class for creating a smooth scrolling effect to all internal links on the page.
-
-license: MIT-style license
-
-authors:
-  - Valerio Proietti
-
-requires:
-  - Core/Slick.Finder
-  - Fx.Scroll
-
-provides: [Fx.SmoothScroll]
-
-...
-*/
-
-Fx.SmoothScroll = new Class({
-
-	Extends: Fx.Scroll,
-
-	options: {
-		axes: ['x', 'y']
-	},
-
-	initialize: function(options, context){
-		context = context || document;
-		this.doc = context.getDocument();
-		this.parent(this.doc, options);
-
-		var win = context.getWindow(),
-			location = win.location.href.match(/^[^#]*/)[0] + '#',
-			links = $$(this.options.links || this.doc.links);
-
-		links.each(function(link){
-			if (link.href.indexOf(location) != 0) return;
-			var anchor = link.href.substr(location.length);
-			if (anchor) this.useLink(link, anchor);
-		}, this);
-
-		this.addEvent('complete', function(){
-			win.location.hash = this.anchor;
-			this.element.scrollTo(this.to[0], this.to[1]);
-		}, true);
-	},
-
-	useLink: function(link, anchor){
-
-		link.addEvent('click', function(event){
-			var el = document.id(anchor) || this.doc.getElement('a[name=' + anchor + ']');
-			if (!el) return;
-
-			event.preventDefault();
-			this.toElement(el, this.options.axes).chain(function(){
-				this.fireEvent('scrolledTo', [link, el]);
-			}.bind(this));
-
-			this.anchor = anchor;
-
-		}.bind(this));
-
-		return this;
-	}
 });
 
 /*
